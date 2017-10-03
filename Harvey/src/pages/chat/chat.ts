09f59a7e36fd75415cd1b951761f0cbf3f1cd288
Philip Gonçalves Pencal <gpencal@hotmail.com>
2017-10-02 23:25:58 -0300

    ta ficando bom

--- Harvey/src/pages/chat/chat.ts
+++ Harvey/src/pages/chat/chat.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var ApiAIPlugin: any;
+
 @Component({
   selector: 'page-chat',
   templateUrl: 'chat.html'
@@ -8,6 +10,7 @@ import { NavController } from 'ionic-angular';
 export class ChatPage {
 
   constructor(public navCtrl: NavController) {
+  
 
   }
 
